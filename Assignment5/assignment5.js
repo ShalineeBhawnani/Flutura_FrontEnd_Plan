591e425d277afa52426c283d29df64729a4678fc
Shalinee <shalineebhawnani90.com>
2020-08-07 23:11:24 +0530

    [Shalinee Bhawnani]Refactor Assignment5

---
+++ Assignment5/assignment5.js
@@ -0,0 +1,20 @@
+var slider_img = document.querySelector('.slider-img');
+var images = ['website1.jpeg', 'website2.jpeg', 'website3.jpeg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
