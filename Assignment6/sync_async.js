ce5c1cff025960b9b40d03e80f3685ce69941186
Shalinee <shalineebhawnani90.com>
2020-08-11 00:44:01 +0530

    [Shalinee Bhawnani]Refactor added Async task in  Assignment6

--- Assignment6/sync_async.js
+++ Assignment6/sync_async.js
@@ -1,10 +1,38 @@
 // synchronous
-
+console.log("click");
 const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
-  alert('You clicked me!');
+  alert('you will get paragraph!');
 
   let para = document.createElement('p');
   para.textContent = 'Hello Welcome here';
+  console.log("new-para",para.textContent);
   document.body.appendChild(para);
 });
+
+
+// asynchronous
+// console.log("start here");
+// const btn1 = document.querySelector('#btn');
+// btn1.addEventListener('click', () => {
+//   console.log("called");
+// });
+// console.log("done");
+
+// asynchronous
+console.log ('asynch block');
+let image;
+fetch('time.png').then((response) => {
+  console.log('fetching image & came here)')
+  return response.blob();  //MIME-type
+}).then((myimage) => {
+  let objectURL = URL.createObjectURL(myimage);
+  image = document.createElement('img');
+  image.src = objectURL;
+  document.body.appendChild(image);
+}).then(() => {
+    (console.log ('end block!' + image.src));
+}).catch((error) => {
+  console.log('error on fetching image: ' + error.message);
+});
+
