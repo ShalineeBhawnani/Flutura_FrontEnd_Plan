cb6aeafa54e704361021e215582eee1f11535761
Shalinee <shalineebhawnani90.com>
2020-08-11 09:51:06 +0530

    [Shalinee Bhawnani]Refactor modified Events  Assignment6

--- Assignment6/Events/events.js
+++ Assignment6/Events/events.js
@@ -40,3 +40,38 @@ function MouseOver(elem) {
   function logKey(e) {
     log.textContent += ` ${e.code}`;
   }
+
+//   function is a listener, or the observer.
+  const btn = document.getElementById('subscribe');
+    btn.addEventListener("click", function () {
+        console.log("Button pressed sent msg");
+        alert("brodcast this msg")
+    });
+
+// event bubbling
+
+const bubble1 = document.getElementById('bubble1');
+const bubble2 = document.getElementById('bubble2');
+
+bubble1.addEventListener("click", function() {
+  console.log("bubble1 clicked");
+
+});
+bubble2.addEventListener("click", function() {
+  console.log("bubble2 clicked");
+  
+});
+
+//event capturing
+
+const capture1 = document.getElementById('bubble1')
+const capture2 = document.getElementById('bubble2');
+
+capture1.addEventListener("click", function() {
+  console.log("bubble1 clicked");
+
+});
+capture2.addEventListener("click", function() {
+  console.log("bubble2 clicked");
+  
+},true);
