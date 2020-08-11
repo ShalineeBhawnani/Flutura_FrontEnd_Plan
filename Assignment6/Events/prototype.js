60b8264577268149d3c6f9263e289ef5c2a204e0
Shalinee <shalineebhawnani90.com>
2020-08-12 02:48:54 +0530

    [Shalinee Bhawnani]Add added higher order functions  Assignment6

--- Assignment6/Events/prototype.js
+++ Assignment6/Events/prototype.js
@@ -1,10 +1,32 @@
+
 var Mobile = function (mob_number) {
+  // Instance Member
   this.mob_number = mob_number;
   this.price = 10000;
 };
 var sumsung = new Mobile('RealMe');
 console.log(sumsung)
 
-// prototype
 sumsung.color = 'white';
 console.log(sumsung)
+
+// color wont available for this object
+var nokia = new Mobile('Nokiya');
+console.log(nokia)
+
+//prototype
+// classname.prototype.key = 'value';
+Mobile.prototype.colors = 'pink'; //prototype mwmber
+console.log("prototype",sumsung.colors)
+console.log("prototype",nokia.colors)
+
+
+// higher order functions 
+
+var add = function(firstValue) {
+return function (secondValue) {
+  return secondValue * firstValue
+};
+  
+}
+console.log(add(3)(7))
